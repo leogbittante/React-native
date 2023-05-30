532bf8b0ff2a8c79fa414a673adb2a271455d449
Leonardo <62109173+leogbittante@users.noreply.github.com>
2023-05-30 11:38:02 -0300

    App react-native
    Final version

---
+++ assets/Itens/index.js
@@ -0,0 +1,15 @@
+import abobora from './Abóbora.png'
+import batata from './Batata.png'
+import brocolis from './Brócolis.png'
+import pepino from './Pepino.png'
+import tomate from './Tomate.png'
+
+const Itens = {
+    abobora,
+    batata,
+    brocolis,
+    pepino,
+    tomate
+}
+
+export default Itens;
--- src/mocks/Cesta.js
+++ src/mocks/Cesta.js
@@ -1,5 +1,7 @@
 import logo from '../../assets/logo.png';
 
+import Itens from '../../assets/Itens';
+
 const cesta = {
     topo: {
         titulo: "Detalhe da cesta"
@@ -11,6 +13,31 @@ const cesta = {
         descricao: "Uma cesta com produtos selecionados cuidadosamente da fazenda direto para a sua cozinha",
         preco: "R$40,00",
         botao: "Comprar"
+    },
+    itens: {
+        titulo: "Itens da cesta",
+        lista: [
+            {
+                nome: "Tomate",
+                imagem: Itens.tomate
+            },
+            {
+                nome: "Brócolis",
+                imagem: Itens.brocolis
+            },
+            {
+                nome: "Batata",
+                imagem: Itens.batata
+            },
+            {
+                nome: "Pepino",
+                imagem: Itens.pepino
+            },
+            {
+                nome: "Abóbora",
+                imagem: Itens.abobora
+            },
+        ]
     }
 }
 
